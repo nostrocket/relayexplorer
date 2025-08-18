07fbf04e0afb06c2e2101e37d1b1afb1ea62629a
gsovereignty <gsovereignty@proton.me>
2025-08-18 16:40:23 +0800

    problem: gh pages not working with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/relayexplorer/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
